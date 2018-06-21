c215a6f04404e6d5589280fc89b248ad2859b7f5
mikolaj-bronk <mikolaj.bronk@gmail.com>
2018-06-21 13:55:13 +0200

    add display all items by Guzzle

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,16 +4,28 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
 
 class ClientController extends Controller
 {
+    private $client;
+
+    public function __construct()
+    {
+       $this->client = new Client();
+    }
+
     /**
+     * Display all items
      * @Route("/", name="client")
      */
-    public function index()
+    public function getAll()
     {
+        $response = $this->client->request('GET', 'http://127.0.0.1:8000/items');
+        $items = json_decode($response->getBody());
         return $this->render('client/index.html.twig', [
-            'controller_name' => 'ClientController',
+            'items' => $items
         ]);
     }
 }
